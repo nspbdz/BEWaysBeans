761ba1ba81e0c6cc409f5fdfe836f1bfd473f201
nspbdz <nspbdz@gmail.com>
2021-08-15 01:43:07 +0700

    firs commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 // const app = express();
 // app.use(express.json());
-// app.use("/api/v1", router);
+// app.use("/api/v1", router); 
 
 // app.listen(PORT, () => {
 //   console.log("Server is running on port: ", PORT);
@@ -20,13 +20,14 @@ const cors = require("cors");
 const router = require("./src/routes");
 const path = require("path");
 
-const PORT = 5000; 
+const port = process.env.PORT||4000; 
 
+console.log(port)
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", router);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.listen(PORT, () => {
-  console.log("Server is running on port: ", PORT);
+app.listen(port, () => {
+  console.log("Server is running on port: ", port);
 });
